6ab9ea953bbfa506dac327d52ce93e15c26ddd1e
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-11-25 18:29:54 +0200

    Add rainbow effect

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
+const columns = document.querySelector("#columnsRows")
+columns.addEventListener("click", () => {
+    const value = prompt()
+    playRound(value);
+})
 
 
 
-for (let i = 0; i < 16; i++) {
 
-    const container = document.querySelector('#container');
-    const squareLength = document.createElement("div");
-    squareLength.classList.add("squareLength");
-    squareLength.textContent = `${i}`;
-    container.appendChild(squareLength);
-    console.log(`${i + 1}`)
+function playRound(grid) {
+    for (let i = 0; i < grid && i < 100; i++) {
 
-    for (let i = 0; i < 16; i++) {
-        const squareHeight = document.createElement("div");
-        squareHeight.classList.add("squareHeight");
-        squareHeight.textContent = `${i}`;
-        squareLength.appendChild(squareHeight);
+        const container = document.querySelector('#container');
+        const squareLength = document.createElement("div");
+        squareLength.classList.add("squareLength");
+
+        container.appendChild(squareLength);
+        console.log(`${i + 1}`)
+
+        for (let i = 0; i < grid && i < 100; i++) {
+            const squareHeight = document.createElement("div");
+            squareHeight.classList.add("squareHeight");
+            squareLength.appendChild(squareHeight);
+
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+            squareHeight.addEventListener("mouseover", () => {
+                squareHeight.style.backgroundColor = "#" + randomColor;
+
+            })
+        }
     }
+
+
 }
+
+
+
 
