8f23b39e4438c0b42f423b005b9444527f1686cc
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-11-25 15:54:19 +0200

    Add 16x16 flex boxes

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-for (i = 0; i < 256; i++) {
-    const container = document.querySelector('#container');
 
-    const square = document.createElement("div")
-    square.classList.add("square");
-    square.textContent = i;
-    container.appendChild(square);
+
+
+for (let i = 0; i < 16; i++) {
+
+    const container = document.querySelector('#container');
+    const squareLength = document.createElement("div");
+    squareLength.classList.add("squareLength");
+    squareLength.textContent = `${i}`;
+    container.appendChild(squareLength);
     console.log(`${i + 1}`)
+
+    for (let i = 0; i < 16; i++) {
+        const squareHeight = document.createElement("div");
+        squareHeight.classList.add("squareHeight");
+        squareHeight.textContent = `${i}`;
+        squareLength.appendChild(squareHeight);
+    }
 }
+
